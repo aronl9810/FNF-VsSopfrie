c57f9f5a96138664ee220801e141382e4cf00a61
Aron Lin <aronl9810@hstat.org>
2023-04-12 22:35:21 -0400

    A little miniscript

--- mods/sopfrie/scripts/testlua.lua
+++ mods/sopfrie/scripts/testlua.lua
@@ -1,5 +1,13 @@
 function onSongStart()
     if songName == "calmingdead" then
-        debugPrint("you are funny!!!")
+        makeLuaText("songintro","CalmingDead by TRGGB2", 400, 800, 600)
+        setTextSize("songintro", 30)
+        addLuaText("songintro")
+    end
+end
+
+function onUpdate(elapsed) -- On FPS 
+    if curStep == 30 then
+        removeLuaText("songintro" , destroy == false)
     end
 end
