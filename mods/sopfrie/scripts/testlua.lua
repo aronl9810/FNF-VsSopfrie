7a02aaebdf5d45390227a0290686ba0ad80e7009
Aron Lin <aronl9810@hstat.org>
2023-05-11 17:39:34 -0400

    Functional + Improved Song introduction

--- mods/sopfrie/scripts/testlua.lua
+++ mods/sopfrie/scripts/testlua.lua
@@ -1,13 +1,14 @@
 function onSongStart()
     if songName == "calmingdead" then
-        makeLuaText("songintro","CalmingDead by TRGGB2", 400, 800, 600)
-        setTextSize("songintro", 30)
-        addLuaText("songintro")
+        makeLuaSprite("decal_calmingdead", "calmingdeaddecal",1300,2000)
+        addLuaSprite("decal_calmingdead",true)
     end
 end
 
-function onUpdate(elapsed) -- On FPS 
-    if curStep == 30 then
-        removeLuaText("songintro" , destroy == false)
+function onUpdate(elapsed) -- Think of this like function draw() in P5JS
+    if curStep == 1 then
+        doTweenY("decal_calmingdead", "decal_calmingdead",900,1,linear)
+    elseif curStep == 30 then
+        doTweenX("decal_calmingdead", "decal_calmingdead",2000,1,linear)
     end
 end
