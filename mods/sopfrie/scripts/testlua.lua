b9ddd4745735d2eba1d4743d353e36677f28f08b
Aron Lin <aronl9810@hstat.org>
2024-01-08 13:11:38 -0500

    Seeing if wsl works again

--- mods/sopfrie/scripts/testlua.lua
+++ mods/sopfrie/scripts/testlua.lua
@@ -1,17 +1,17 @@
 -- New Song Introduction
 
-function onSongStart()
-    if songName == "calmingdead" then
-        makeLuaSprite("decal_calmingdead", "calmingdeaddecal",1300,2000)
-        addLuaSprite("decal_calmingdead",true)
+function onSongStart() -- Execute the code as soon as the song starts
+    if songName == "calmingdead" then -- Checks if the song matches the one your playing on
+        makeLuaSprite("decal_calmingdead", "calmingdeaddecal",1300,2000) -- Make the image
+        addLuaSprite("decal_calmingdead",true) -- Spawns the image
     end
 end
 
 function onUpdate(elapsed) -- Think of this like function draw() in P5JS
     if curStep == 1 then
-        doTweenY("decal_calmingdead", "decal_calmingdead",900,1,linear)
+        doTweenY("decal_calmingdead", "decal_calmingdead",900,1,linear) -- Move the image upward
     elseif curStep == 30 then
-        doTweenX("decal_calmingdead", "decal_calmingdead",2000,1,linear)
+        doTweenX("decal_calmingdead", "decal_calmingdead",2000,1,linear) -- Move the image to the right. Making it disappear!
     end
 end
 
